1d306a5c4f89d5ca5d75cf876e85cd7dee3f6f95
NikitaBarashkov <nik96barashkov@yandex.by>
2022-08-12 21:23:55 +0300

    feat: add part of function (Fri, 12 Aug 2022, 21:23)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  // const closePair = {
+  //   '(': ')',
+  //   '{': '}',
+  //   '[': ']',
+  //   '|': '|',
+  // }
+
+  const steck = []
+
+  for(let i = 0; i < str.length; i++){
+    let currentSymbol = str[i];
+   
+    for(let arr of bracketsConfig){
+      let openBrackets = arr[0];
+      let closeBrackets = arr[1]
+    
+      if(currentSymbol === openBrackets){
+        let lastSymbol = steck[steck.length - 1];
+        if(steck.length > 0 && lastSymbol === '|' && currentSymbol === '|'){
+          steck.pop(lastSymbol);
+        } else {
+          steck.push(currentSymbol);
+        }
+      } else {
+        
+          // if(steck.length === 0){
+          //   return false;
+          // }
+
+          let lastSymbol = steck[steck.length - 1];
+
+          if(currentSymbol === closeBrackets && lastSymbol === openBrackets){
+            steck.pop(lastSymbol); 
+          }
+        }
+      }
+    }
+
+    if(steck.length === 0){
+      return true;
+    } else {
+      return false;
+    }
+  
 }
